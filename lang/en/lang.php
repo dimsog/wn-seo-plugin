7b859e9603126f8ff10b5859d29d95851111bca2
Dmitry R <dimsog@gmail.com>
2023-01-09 16:23:09 +0300

    feat: init models

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -3,18 +3,25 @@
 return [
     'plugin' => [
         'name' => 'Seo',
-        'description' => 'Seo plugin for WinterCMS'
+        'description' => 'Seo plugin for WinterCMS',
     ],
     'components' => [
         'seo' => [
-            'name' => 'Seo'
-        ]
+            'name' => 'Seo',
+        ],
     ],
     'settings' => [
         'name' => 'Seo',
         'header_and_footer' => 'Head and Footer',
         'header' => 'Head code',
         'footer' => 'Footer code',
-        'robots' => 'Robots.txt'
-    ]
+        'robots' => 'Robots.txt',
+    ],
+    'models' => [
+        'general' => [
+            'id' => 'ID',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ],
+    ],
 ];
